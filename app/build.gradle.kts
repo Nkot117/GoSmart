287ca8b29fb9c98eb132d309f743f6351fa04191
Nkot117 <kumakura117@gmail.com>
2025-11-03 14:38:30 +0900

    Hiltエントリポイントを追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -56,4 +59,8 @@ dependencies {
     implementation(libs.androidx.material3)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 }
---
+++ app/src/main/java/com/nkot117/smartgo/SmartGoApp.kt
@@ -0,0 +1,7 @@
+package com.nkot117.smartgo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SmartGoApp : Application()
