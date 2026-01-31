ce0acacf29bc85035012a8bdf7eafec569ae0fbf
Nkot117 <kumakura117@gmail.com>
2026-01-31 16:19:13 +0900

    refactor(data): switch ReminderSettingsRepository to use IO dispatcher

--- core/data/src/main/java/com/nkot117/core/data/repository/ReminderSettingsRepositoryImpl.kt
+++ core/data/src/main/java/com/nkot117/core/data/repository/ReminderSettingsRepositoryImpl.kt
@@ -1,23 +1,32 @@
 package com.nkot117.core.data.repository
 
 import com.nkot117.core.data.datastore.ReminderSettingsDataSource
+import com.nkot117.core.data.di.IODispatcher
 import com.nkot117.core.domain.model.ReminderTime
 import com.nkot117.core.domain.repository.ReminderSettingsRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ReminderSettingsRepositoryImpl @Inject constructor(
     private val dataSource: ReminderSettingsDataSource,
+    @param:IODispatcher private val io: CoroutineDispatcher,
 ) : ReminderSettingsRepository {
     override fun observeTime(): Flow<ReminderTime> {
-        return dataSource.observeTime()
+        return dataSource.observeTime().flowOn(io)
     }
 
     override suspend fun getTime(): ReminderTime {
-        return dataSource.getTime()
+        return withContext(io) {
+            dataSource.getTime()
+        }
     }
 
     override suspend fun saveTime(time: ReminderTime) {
-        dataSource.saveTime(time)
+        withContext(io) {
+            dataSource.saveTime(time)
+        }
     }
 }
