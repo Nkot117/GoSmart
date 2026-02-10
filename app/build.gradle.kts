68cca8c2370800197fb3dd24e0d4489e9b9b2c44
Nkot117 <kumakura117@gmail.com>
2026-02-11 00:23:42 +0900

    chore: fix pass

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
             val storeFilePath =
                 System.getenv("SIGNING_STORE_FILE") ?: "keystore/smartgo-internal.jks"
 
-            storeFile = file(storeFilePath)
+            storeFile = rootProject.file(storeFilePath)
             storePassword = System.getenv("SIGNING_STORE_PASSWORD")
             keyAlias = System.getenv("SIGNING_KEY_ALIAS")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
