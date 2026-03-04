12cd4e066e93bf39ca2d0c92b95067059f2ac660
Nkot117 <kumakura117@gmail.com>
2026-03-04 18:45:17 +0900

    chore: Preparation for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.nkot117.smartgo"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
