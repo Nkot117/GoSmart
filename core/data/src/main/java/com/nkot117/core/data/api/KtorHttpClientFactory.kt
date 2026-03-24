efa57af021c6b2426c0bee90a81ef24c15897bf6
Nkot117 <kumakura117@gmail.com>
2026-03-24 18:14:05 +0900

    feat(api): add Ktor client plugin configuration

--- core/data/src/main/java/com/nkot117/core/data/api/KtorHttpClientFactory.kt
+++ core/data/src/main/java/com/nkot117/core/data/api/KtorHttpClientFactory.kt
@@ -2,7 +2,35 @@ package com.nkot117.core.data.api
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
 
 class KtorHttpClientFactory {
-    fun create(): HttpClient = HttpClient(OkHttp)
+    fun create(): HttpClient = HttpClient(OkHttp) {
+        expectSuccess = true
+
+        install(ContentNegotiation) {
+            json()
+        }
+
+        // TODO: 本番ではLogLevel.NONEにする
+        install(Logging) {
+            level = LogLevel.BODY
+        }
+
+        install(HttpRequestRetry) {
+            retryOnServerErrors(maxRetries = 3)
+            exponentialDelay()
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 15_000
+            connectTimeoutMillis = 10_000
+            socketTimeoutMillis = 15_000
+        }
+    }
 }
