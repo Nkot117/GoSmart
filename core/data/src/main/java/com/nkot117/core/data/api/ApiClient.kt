4a1e370763cbaa2335bdae152f5f662d220be3b1
Nkot117 <kumakura117@gmail.com>
2026-03-24 19:31:48 +0900

    feat(api): make ApiClient get/post generic

--- core/data/src/main/java/com/nkot117/core/data/api/ApiClient.kt
+++ core/data/src/main/java/com/nkot117/core/data/api/ApiClient.kt
@@ -1,15 +1,15 @@
 package com.nkot117.core.data.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.post
-import io.ktor.client.statement.bodyAsText
 
-class ApiClient(private val httpClient: HttpClient) {
-    suspend fun get(url: String, block: HttpRequestBuilder.() -> Unit = {}): String =
-        httpClient.get(url, block).bodyAsText()
+class ApiClient(@PublishedApi internal val httpClient: HttpClient) {
+    suspend inline fun <reified T> get(url: String, block: HttpRequestBuilder.() -> Unit = {}): T =
+        httpClient.get(url, block).body()
 
-    suspend fun post(url: String, block: HttpRequestBuilder.() -> Unit = {}): String =
-        httpClient.post(url, block).bodyAsText()
+    suspend inline fun <reified T> post(url: String, block: HttpRequestBuilder.() -> Unit = {}): T =
+        httpClient.post(url, block).body()
 }
