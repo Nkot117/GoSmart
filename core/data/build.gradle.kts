fc2b7733e00287e8fc61f818b93089a443458f3d
Nkot117 <kumakura117@gmail.com>
2026-03-24 18:14:05 +0900

    feat(api): add Ktor client plugin configuration

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -60,4 +60,10 @@ dependencies {
     // Ktor
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.logging)
+
+    // Kotlinx Serialization
+    implementation(libs.kotlinx.serialization.json)
 }
