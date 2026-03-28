27fed5915fc963f2a0c9cc2e83b2ac94ed573ddc
Nkot117 <kumakura117@gmail.com>
2026-03-28 18:01:11 +0900

    feat(domain): Add GetCurrentLocationDailyWeatherTypeUseCase

---
+++ core/domain/src/main/java/com/nkot117/core/domain/usecase/weather/GetCurrentLocationDailyWeatherTypeUseCase.kt
@@ -0,0 +1,23 @@
+package com.nkot117.core.domain.usecase.weather
+
+import com.nkot117.core.domain.model.WeatherType
+import com.nkot117.core.domain.repository.WeatherInfoRepository
+import javax.inject.Inject
+
+class GetCurrentLocationDailyWeatherTypeUseCase @Inject constructor(
+    private val weatherInfoRepository: WeatherInfoRepository
+) {
+    suspend operator fun invoke(): WeatherType {
+        // TODO: 端末から現在地の緯度経度を取得し、Repositoryに渡すように修正する
+        val dailyWeatherInfo = weatherInfoRepository.getCurrentLocationDailyWeatherInfo(
+            latitude = 0.0,
+            longitude = 0.0
+        )
+        return dailyWeatherInfo.weatherCode.toWeatherType()
+    }
+}
+
+private fun Int.toWeatherType(): WeatherType = when (this) {
+    0, 1, 2 -> WeatherType.SUNNY
+    else -> WeatherType.RAINY
+}
